f3cdc26995d96f781bf42f8b8afcd9628f6de064
bladeofgods <bladeofgods@163.com>
2020-09-24 14:06:32 +0800

    drawer layout

--- lib/page/main/drawer/drawer_page.dart
+++ lib/page/main/drawer/drawer_page.dart
@@ -5,6 +5,7 @@
 
 import 'package:cloud_music/base_framework/view_model/app_model/user_view_model.dart';
 import 'package:cloud_music/base_framework/widget_state/page_state.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,12 +18,15 @@ class DrawerPage extends PageState with SingleTickerProviderStateMixin{
   double maxDrawerWidth;
   double flag;
   double startPos;
-  
+
+  ScrollController scrollController;
+
   @override
   void initState() {
     maxDrawerWidth = getWidthPx(650);
     startPos = -getWidthPx(650);
     flag = startPos/3;
+    scrollController = ScrollController();
     controller = AnimationController(vsync: this,duration: Duration(milliseconds: 500));
     animation = Tween<double>(begin: startPos,end: 0).animate(controller);
     super.initState();
@@ -50,6 +54,12 @@ class DrawerPage extends PageState with SingleTickerProviderStateMixin{
       child: Consumer<UserViewModel>(
         builder: (ctx,userModel,child){
           return GestureDetector(
+            onHorizontalDragStart: dragStart,
+            onHorizontalDragUpdate: dragUpdate,
+            onHorizontalDragEnd: dragEnd,
+            onVerticalDragStart: dragStartV,
+            onVerticalDragUpdate: dragUpdateV,
+            onVerticalDragEnd: dragEndV,
             onTap: (){
               //todo
               closeDrawer(0.0);
@@ -73,17 +83,44 @@ class DrawerPage extends PageState with SingleTickerProviderStateMixin{
   }
   
   Widget drawer(){
-    return GestureDetector(
-      onHorizontalDragStart: dragStart,
-      onHorizontalDragUpdate: dragUpdate,
-      onHorizontalDragEnd: dragEnd,
-      child: Container(
-        color: Colors.white,
-        width: getWidthPx(650),height: getHeightPx(1334),
-        child: SingleChildScrollView(),
+    return Container(
+      width: getWidthPx(650),height: getHeightPx(1334),
+      child: SingleChildScrollView(
+        controller: scrollController,
+        child: Stack(
+          children: <Widget>[
+            ///vip
+            vipWidget(),
+            ///zone
+            zoneWidget(),
+          ],
+        ),
       ),
     );
     
+  }
+
+  Widget vipWidget(){
+    return Container(
+      width: maxDrawerWidth,
+      height: getWidthPx(500),
+      color: Color.fromRGBO(105, 105, 105, 1),
+    );
+
+  }
+
+  Widget zoneWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: getWidthPx(400)),
+      width: maxDrawerWidth,height: getHeightPx(1400),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(getHeightPx(30)),
+            topRight: Radius.circular(getHeightPx(30)))
+      ),
+      //child: Text('test'),
+    );
+
   }
 
   Offset startPoint;
@@ -104,7 +141,6 @@ class DrawerPage extends PageState with SingleTickerProviderStateMixin{
   }
 
   dragEnd(DragEndDetails details){
-    debugPrint('$startPos  -----$flag');
     if(startPos < flag){
       closeDrawer(startPos);
     }else{
@@ -127,6 +163,20 @@ class DrawerPage extends PageState with SingleTickerProviderStateMixin{
     double time = (maxDrawerWidth-(start.abs()))/maxDrawerWidth;
     controller.forward(from: time);
   }
+///vertical
+
+  dragStartV(DragStartDetails details){}
+
+  dragUpdateV(DragUpdateDetails details){
+    final delta = details.delta;
+    final position = scrollController.position;
+    final target = position.pixels-delta.dy;
+    scrollController.jumpTo(target);
+
+
+  }
+
+  dragEndV(DragEndDetails details){}
 
 
 }
